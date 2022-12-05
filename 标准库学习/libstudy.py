68fc5f18f22b6c267b0f2639a24e76377cd0acc1
张建广 <simonzhang84@163.com>
2022-12-05 12:08:55 +0800

    magic python

--- 标准库学习/libstudy.py
+++
@@ -1,6 +0,0 @@
-
-def libstudy(a):
-    print(exec(a).__name__)
-    print(exec(a).__dict__)
-    print(help(exec(a)))
-    print(dir(exec(a)))
--- 标准库学习/randoms.py
+++
@@ -1,7 +0,0 @@
-import random
-
-t = [1, 2, 3, 4, 5, 6]
-random.shuffle(t)
-
-var = random.Random
-print(var)
